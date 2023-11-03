b1ba7391a093581c6ff45d7997aba8e07819fd16
Yoshihiro Tanaka <contact@cordea.jp>
2023-11-03 13:34:23 +0900

    initial commit :)

---
+++ bin/dart_spell_checker.dart
@@ -0,0 +1,5 @@
+import 'package:dart_spell_checker/dart_spell_checker.dart' as dart_spell_checker;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_spell_checker.calculate()}!');
+}
