07037e7a46075d94fb94e5eb4a986dd849f30e31
Yoshihiro Tanaka <contact@cordea.jp>
2023-11-03 13:54:26 +0900

    list files

--- bin/dart_spell_checker.dart
+++ bin/dart_spell_checker.dart
@@ -1,5 +1,22 @@
-import 'package:dart_spell_checker/dart_spell_checker.dart' as dart_spell_checker;
+import 'dart:io';
+
+import 'package:dart_spell_checker/dart_spell_checker.dart';
 
 void main(List<String> arguments) {
-  print('Hello world: ${dart_spell_checker.calculate()}!');
+  final path = arguments.first;
+  final dir = Directory(path);
+  final Iterable<File> files;
+  if (dir.statSync().type == FileSystemEntityType.file) {
+    if (path.endsWith('.dart')) {
+      files = [File(path)];
+    } else {
+      throw ArgumentError();
+    }
+  } else {
+    files = dir
+        .listSync(recursive: true)
+        .whereType<File>()
+        .where((e) => e.path.endsWith('.dart'));
+  }
+  check(files);
 }
--- lib/dart_spell_checker.dart
+++ lib/dart_spell_checker.dart
@@ -1,3 +1,3 @@
-int calculate() {
-  return 6 * 7;
-}
+import 'dart:io';
+
+void check(Iterable<File> files) {}
